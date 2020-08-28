eafe97cfdeecec65073cc088dc7a414a594e5a05
Joe Sweeney <joe.sweeney224@gmail.com>
2020-08-27 21:01:12 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import { serve } from 'https://deno.land/std@0.66.0/http/server.ts'
+
+const server = serve({
+  port: 3000
+})
+
+console.log('server started.')
+
+for await (const req of server) {
+  if (req.url == '/') {
+    req.respond({
+      status: 200,
+      body: await Deno.open('./frontend/index.html')
+    })
+  }
+}
